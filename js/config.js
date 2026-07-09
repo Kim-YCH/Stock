3890dafe9e137eb5b9783628a0df4383bb01b7bd
Kim-YCH <sss881015@gmail.com>
2026-07-09 21:30:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // 請改成你的 Apps Script Web App URL。
 // 空白時會使用前端假資料，方便先看 UI。
-const API_BASE_URL = "https://script.google.com/macros/s/AKfycbya-poMopCir9dGRg0JhVNDoGWYtOmuoNu4kyjrEfcn8TwUqxhXmrlMAb_740XvtJoS/exec";
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycby4swalZFOn4xImF5kOgVZlsD_ElorJmO9qeLeks5tANUfa6qdU4jdXLCjkgGQALycK/exec";
 
 // 若你在 Apps Script 的 CONFIG.API_TOKEN 有設定，這裡也要填一樣。
 // 個人使用可以先留空。
