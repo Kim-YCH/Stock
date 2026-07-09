3eaffa5538aa5b60aaeb667da79ec1c6a0bc7281
Kim-YCH <sss881015@gmail.com>
2026-07-09 16:26:11 +0800

    Update Apps Script API URL

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // 請改成你的 Apps Script Web App URL。
 // 空白時會使用前端假資料，方便先看 UI。
-const API_BASE_URL = "https://script.google.com/macros/s/AKfycbwthXjvRzlApwTFR8j1_eLqD12dsXwPcqj4Oac6weZMu-GwgZkUYHjzLDWIErGCDmPY/exec";
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycbzWsyfgC2uZtnxfKHSjXcLBf8CriYzW5xqiTQME0JX8R5ZBlLemAW1t7RMx_A5lRjUi/exec";
 
 // 若你在 Apps Script 的 CONFIG.API_TOKEN 有設定，這裡也要填一樣。
 // 個人使用可以先留空。
