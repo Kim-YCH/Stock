4713f1d8e4e3b5162e576b3b142d1f8c47247fe7
Kim-YCH <sss881015@gmail.com>
2026-07-14 13:48:36 +0800

    Improve dashboard UX trading flow and performance

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://script.google.com/macros/s/AKfycbzXkzQ6dL1nDQLoxmI736tZn3DYdgcOnWz0nrV_gmtN--Bwe9bGLwvg6MfMggVB82fT/exec"
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycbwDh6A3gg1tmakYM7i0_FdjuitulXxo-kNwvwcvfv_cj6G5qcO_-RHvgC9I3-UbkcHq/exec"
 const API_TOKEN = "";
 
 const APP_VERSION = "v11.5";
